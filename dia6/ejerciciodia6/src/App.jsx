460197bdfd4818204edb06b5b651a2f866db8ac2
Jlquintero97 <joseluisql97@hotmail.com>
2025-05-04 18:43:55 +0200

    first commit

---
+++ dia6/ejerciciodia6/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/header'
+import Main from './components/main'
+
+function App() {
+
+  return (
+    <>
+      <Header name="Hola paco" />
+      <Main />
+    </>
+  )
+}
+
+export default App
---
+++ dia6/ejerciciodia6/src/components/main.jsx
@@ -0,0 +1,12 @@
+import Calc from './calc'
+
+function main(){
+   return (
+    <>
+    <h1>¡Hola Mundo!</h1>
+    <Calc/>
+    </>
+   )
+
+}
+export default main
